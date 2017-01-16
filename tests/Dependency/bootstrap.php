c7b1204dc82e02cf3b3268f64a4b5b5c3f0d24a7
Jan Pecha <janpecha@email.cz>
2017-01-16 19:37:05 +0100

    Composer: updated nette/tester

--- tests/Dependency/bootstrap.php
+++ tests/Dependency/bootstrap.php
@@ -1,7 +1,3 @@
 <?php
-require __DIR__ . '/../../vendor/nette/tester/Tester/bootstrap.php';
 
-if (extension_loaded('xdebug'))
-{
-	Tester\CodeCoverage\Collector::start(__DIR__ . '/../coverage.dat');
-}
+require __DIR__ . '/../../vendor/nette/tester/Tester/bootstrap.php';
